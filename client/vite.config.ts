15043565f3bc21044ffac64d9079f57dbea16621
Dheerajydv <dy81035@gmail.com>
2025-08-10 23:36:19 +0530

    proxy changed for frontend

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,18 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:8000",
-        secure: false,
-        changeOrigin: true,
-        ws: true,
-      },
+    plugins: [react(), tailwindcss()],
+    server: {
+        proxy: {
+            "/api": {
+                target: "https://omega-p95o.onrender.com",
+                secure: false,
+                changeOrigin: true,
+                ws: true,
+            },
+        },
     },
-  },
-})
+});
