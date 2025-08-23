612d136924ff014edb9a62dc6eefc70eabae2704
Dheerajydv <dy81035@gmail.com>
2025-08-23 22:06:17 +0530

    Final loader color change

--- client/src/components/Loader.tsx
+++ client/src/components/Loader.tsx
@@ -1,4 +1,4 @@
 const Loader = () => {
-    return <span className="loading loading-spinner text-primary"></span>;
+    return <span className="loading loading-spinner text-secondary"></span>;
 };
 export default Loader;
